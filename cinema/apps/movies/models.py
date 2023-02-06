033150aae1d8d9afa96e052f4c9d416390134e61
h_samsami <hamidreza.samsami@gmail.com>
2023-02-06 18:35:56 +0330

    add movie model

--- cinema/apps/movies/models.py
+++ cinema/apps/movies/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from apps.rooms.models import Room
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    poster = models.ImageField(upload_to='posters/')
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
